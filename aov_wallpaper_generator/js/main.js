d825420e1950606e44c6f0d61f54b3c26a189077
Rendy Harimayco <rendyharimayco@gmail.com>
2019-02-15 17:00:00 +0700

    add utils

--- aov_wallpaper_generator/js/main.js
+++ aov_wallpaper_generator/js/main.js
@@ -1,6 +1,8 @@
-const garena_cdn = 'https://cdngarenanow-a.akamaihd.net/webmain/static/resource/wallpaper/hero/';
+const garena_wp = 'https://cdngarenanow-a.akamaihd.net/webmain/static/resource/wallpaper/';
+const garena_cdn = garena_wp + 'hero/';
 const wp_data = [{"name":"toro","value":"toro","tag":"tank","skinsCount":3,"bgCount":3},{"name":"raz","value":"raz","tag":"mage","skinsCount":4,"bgCount":4},{"name":"zephys","value":"zephys","tag":"warrior","skinsCount":4,"bgCount":4},{"name":"veera","value":"veera","tag":"mage","skinsCount":5,"bgCount":5},{"name":"violet","value":"violet","tag":"archer","skinsCount":7,"bgCount":7},{"name":"yorn","value":"yorn","tag":"archer","skinsCount":5,"bgCount":5},{"name":"omega","value":"omega","tag":"tank","skinsCount":2,"bgCount":2},{"name":"butterfly","value":"butterfly","tag":"assassin","skinsCount":7,"bgCount":7},{"name":"mina","value":"mina","tag":"tank","skinsCount":3,"bgCount":3},{"name":"azzenka","value":"azzenka","tag":"mage","skinsCount":3,"bgCount":3},{"name":"lubu","value":"lubu","tag":"warrior","skinsCount":6,"bgCount":6},{"name":"zanis","value":"zanis","tag":"warrior","skinsCount":5,"bgCount":5},{"name":"valhein","value":"valhein","tag":"archer","skinsCount":6,"bgCount":6},{"name":"thane","value":"thane","tag":"tank","skinsCount":3,"bgCount":3},{"name":"ilumia","value":"ilumia","tag":"mage","skinsCount":3,"bgCount":3},{"name":"lauriel","value":"lauriel","tag":"mage","skinsCount":5,"bgCount":5},{"name":"nakroth","value":"nakroth","tag":"assassin","skinsCount":5,"bgCount":5},{"name":"diaochan","value":"diaochan","tag":"mage","skinsCount":4,"bgCount":4},{"name":"aleister","value":"aleister","tag":"mage","skinsCount":3,"bgCount":3},{"name":"wukong","value":"wukong","tag":"assassin","skinsCount":4,"bgCount":4},{"name":"krixi","value":"krixi","tag":"mage","skinsCount":6,"bgCount":6},{"name":"gildur","value":"gildur","tag":"tank","skinsCount":2,"bgCount":2},{"name":"kahli","value":"kahli","tag":"mage","skinsCount":4,"bgCount":4},{"name":"chaugnar","value":"chaugnar","tag":"support","skinsCount":3,"bgCount":3},{"name":"ormarr","value":"ormarr","tag":"warrior","skinsCount":4,"bgCount":4},{"name":"alice","value":"alice","tag":"support","skinsCount":3,"bgCount":3},{"name":"mganga","value":"mganga","tag":"mage","skinsCount":3,"bgCount":3},{"name":"maloch","value":"maloch","tag":"warrior","skinsCount":3,"bgCount":3},{"name":"taara","value":"taara","tag":"tank","skinsCount":3,"bgCount":3},{"name":"preyta","value":"preyta","tag":"mage","skinsCount":3,"bgCount":3},{"name":"cresht","value":"cresht","tag":"tank","skinsCount":3,"bgCount":3},{"name":"fennik","value":"fennik","tag":"archer","skinsCount":3,"bgCount":3},{"name":"grakk","value":"grakk","tag":"tank","skinsCount":3,"bgCount":3},{"name":"natalya","value":"natalya","tag":"mage","skinsCount":5,"bgCount":5},{"name":"lumburr","value":"lumburr","tag":"support","skinsCount":2,"bgCount":2},{"name":"jinna","value":"jinna","tag":"mage","skinsCount":3,"bgCount":3},{"name":"payna","value":"payna","tag":"support","skinsCount":2,"bgCount":2},{"name":"arthur","value":"arthur","tag":"warrior","skinsCount":4,"bgCount":4},{"name":"kriknak","value":"kriknak","tag":"assassin","skinsCount":3,"bgCount":3},{"name":"airi","value":"airi","tag":"warrior","skinsCount":6,"bgCount":6},{"name":"batman","value":"batman","tag":"assassin","skinsCount":2,"bgCount":2},{"name":"slimz","value":"slimz","tag":"archer","skinsCount":3,"bgCount":3},{"name":"skud","value":"skud","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"zuka","value":"zuka","tag":"warrior","skinsCount":5,"bgCount":5},{"name":"murad","value":"murad","tag":"assassin","skinsCount":6,"bgCount":6},{"name":"telannas","value":"telannas","tag":"archer","skinsCount":5,"bgCount":5},{"name":"Zill","value":"qier","tag":"mage","skinsCount":2,"bgCount":4},{"name":"ignis","value":"ignis","tag":"mage","skinsCount":2,"bgCount":2},{"name":"joker","value":"joker","tag":"archer","skinsCount":2,"bgCount":2},{"name":"superman","value":"superman","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"kilgroth","value":"kilgroth","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"xeniel","value":"xeniel","tag":"tank","skinsCount":3,"bgCount":3},{"name":"arduin","value":"arduin","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"Ryoma","value":"longma","tag":"warrior","skinsCount":4,"bgCount":4},{"name":"astrid","value":"aicuisi","tag":"warrior","skinsCount":3,"bgCount":3},{"name":"moren","value":"moen","tag":"archer","skinsCount":2,"bgCount":2},{"name":"wonderwoman","value":"wonderwoman","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"tulen","value":"tulun","tag":"mage","skinsCount":5,"bgCount":5},{"name":"lindis","value":"lindi","tag":"archer","skinsCount":3,"bgCount":3},{"name":"timi","value":"timi","tag":"support","skinsCount":2,"bgCount":2},{"name":"omen","value":"yecha","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"max","value":"maikesi","tag":"warrior","skinsCount":3,"bgCount":3},{"name":"liliana","value":"lilian","tag":"mage","skinsCount":4,"bgCount":4},{"name":"wisp","value":"lingling","tag":"archer","skinsCount":3,"bgCount":3},{"name":"arum","value":"airui","tag":"tank","skinsCount":3,"bgCount":3},{"name":"rourke","value":"luoke","tag":"warrior","skinsCount":3,"bgCount":3},{"name":"roxie","value":"nuokexi","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"baldum","value":"bodun","tag":"tank","skinsCount":2,"bgCount":2},{"name":"marja","value":"majia","tag":"mage","skinsCount":2,"bgCount":2},{"name":"emily","value":"aimili","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"annete","value":"annaite","tag":"support","skinsCount":2,"bgCount":2},{"name":"y'bneth","value":"gumu","tag":"tank","skinsCount":2,"bgCount":2},{"name":"elsu","value":"su","tag":"archer","skinsCount":3,"bgCount":3},{"name":"richter/Riktor","value":"ruike","tag":"warrior","skinsCount":2,"bgCount":2},{"name":"Sephera","value":"lan","tag":"mage","skinsCount":2,"bgCount":2},{"name":"quillen","value":"kuilun","tag":"assassin","skinsCount":2,"bgCount":2},{"name":"Darcy","value":"daerxi","tag":"mage","skinsCount":2,"bgCount":2},{"name":"Capheny","value":"kafenni","tag":"archer","skinsCount":2,"bgCount":2},{"name":"Verez","value":"feilei","tag":"warrior","skinsCount":2,"bgCount":2}];
 const fonts = ['Staatliches', 'Bloodseeker', 'Miss Fajardose', 'Righteous', 'Luckiest Guy', 'Passion One', 'Bangers', 'Black Ops One', 'Arial', 'Arial Black', 'Georgia', 'Trebuchet MS', 'Courier New', 'Lucida Console', 'Comic Sans MS'];
+const custom_utils = ['aov-logo-1','aov-logo-2','aov-logo-3','aov-logo-4','aov-logo-5','fire-splash','splash-blue','tencent-1'];
 
 window.mobileAndTabletcheck = function() {
   var check = false;
@@ -34,11 +36,31 @@ $(function(){
 	render_thumbnails('bg', $('#bg-wrapper > div'), '.jpg');
 	//render skins thumb
 	render_thumbnails('skins', $('#skin-wrapper > div'));
+  //render custom util;
+  render_custom_utils($('#util'));
+  //render util;
+  render_utils($('#util'), 14);
   //renderfonts
   render_fonts_select($('#text-font'));
 });
 
+function render_custom_utils(container){
+  var html = '';
+  var i = 1;
+  $.each(custom_utils, function(i,data){
+    html = html + '<img src="images/thumbs/'+data+'.jpg" data-source="images/'+data+'.png" data-sub="custom-utils" height="70" /> ';
+    i++;
+  });
+  container.append(html);
+}
 
+function render_utils(container, count = 0){
+  var html = '';
+  for(var x=count;x>=1;x--){
+    html = html + '<img src="'+garena_wp+'tw-title-thumb/'+x+'.jpg" data-sub="tw-title" width="140" data-number="'+x+'" /> ';
+  }
+  container.append(html);
+}
 
 function render_thumbnails(type, container){
 	var html = '';
@@ -172,13 +194,23 @@ stage.add(layer);
     		var sub = $(this).data('sub');
     		var hero = $(this).data('hero');
     		var number = $(this).data('number');
+        var source = $(this).data('source');
     		var ext = '.png';
     		var baseon = 'width';
     		if(sub == 'bg'){
     			ext = '.jpg';
     			baseon = 'height';
     		}
-    		var file = garena_cdn + hero +'/'+ sub +'/'+number+ext;
+
+        var file = '';
+        if(sub == 'tw-title'){
+          file = garena_wp + sub +'/'+number+ext;
+        }else if(sub === 'custom-utils'){
+          file = source;
+        }else{
+          file = garena_cdn + hero +'/'+ sub +'/'+number+ext;
+        }
+    		
 
     		add_image(file, baseon);
     	});
